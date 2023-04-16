bab6a9b9e432db89470a000d3a706395314f3b30
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-04-16 14:36:29 +0300

    feat: описать схему и модель сущности movie

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,7 @@
 const EMAIL_REGEX = /.+@.+\..+/;
+const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
 module.exports = {
   EMAIL_REGEX,
+  URL_REGEX,
 };
